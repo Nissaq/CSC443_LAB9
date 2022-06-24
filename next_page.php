0dbfacdb2ac4ffaa18dcd6bd6c36c7a1bfda3ca7
Nissaq <2020470222@student.uitm.edu.my>
2022-06-24 15:03:02 +0800

    next_page.php

--- next_page.php
+++ next_page.php
@@ -2,16 +2,25 @@
     session_start();
     echo "<html>";
 
-    $sid = session_id();
-    echo "Session ID returned by session_id(): ".@sid."\n";
-    $sid = SID;
-    echo "Session ID returned by SID: ".$sid."\n";
+    $queryString = $_SERVER["QUERY_STRING"];
+    echo "Query string of the incoming URL: ".$queryString."\n';
+
+    echo "Cookies received:\n";
+    foreach ($_COOKIE as $name => $value)
+    {
+        echo "$name = $value\n";
+    }
+
+    //$sid = session_id();
+    // echo "Session ID returned by session_id(): ".$sid."\n <br>";
+    // $sid = SID;
+    // echo "Session ID returned by SID: ".$sid."\n <br>";
 
     $myLogin = $_SESSION["myLogin"];
-    echo "Value of myLogin has been retrieved: ".$myLogin."\n";
+    echo "Value of myLogin has been retrieved: ".$myLogin."\n <br>";
     
     $myColor = $_SESSION["myColor"];
-    echo "Value of myColor has been retrieved: ".$myColor."\n";
+    echo "Value of myColor has been retrieved: ".$myColor."\n <br>";
 
     echo "</html>\n";
 ?>
