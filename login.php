dcab2738738eb27dc614263e5be9fc244abf5391
Nissaq <2020470222@student.uitm.edu.my>
2022-06-24 15:41:12 +0800

    login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
 
     if (isset ($_COOKIE[session_name()]))
     {
-        setcookie(session_name(), '', time()-42000, '/';
+        setcookie(session_name(), '', time()-42000), '/';
     }
 
     session_destroy();
